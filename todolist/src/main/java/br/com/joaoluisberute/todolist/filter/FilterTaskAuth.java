6c6d1d36cbb5445bd1e82dbbdb29adb35c4c4ebd
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2024-01-26 01:41:31 -0300

    Update FilterTaskAuth.java

--- todolist/src/main/java/br/com/joaoluisberute/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/joaoluisberute/todolist/filter/FilterTaskAuth.java
@@ -1,10 +1,14 @@
 package br.com.joaoluisberute.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.joaoluisberute.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,12 +18,65 @@ import lombok.NonNull;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
-        System.out.println(request.getHeader("autorization"));
-        filterChain.doFilter(request, response);
+        var servletPath = request.getServletPath();
+
+        if(servletPath.equals("/Task/CreateTask")){
+
+            // Guarda na variavel authHeaderAuthorization o tipo de autorização e as informações da autorização em si na forma de BASE64 ("TypeAuth"+" "+"d5fs4654fvsdfzx342#$")
+            var authHeaderAuthorization = request.getHeader("Authorization");
+
+            // Guarda na variavel authorizationParsedToBase64Only 
+            var authorizationParsedToBase64Only = authHeaderAuthorization.substring("Basic".length()).trim();
+
+            var authorizationParsedToString = new String(Base64.getDecoder().decode(authorizationParsedToBase64Only));
+
+            String[] arrayWithUserAndPassword = authorizationParsedToString.split(":");
+
+            System.out.println(authorizationParsedToString);
+            
+            var username = arrayWithUserAndPassword[0];
+            var password = arrayWithUserAndPassword[1];
+
+            System.out.println("Username:");
+            System.out.println(username.length());
+            
+            System.out.println("Password:");
+            System.out.println(password);
+            
+                
+            if(username.length() != 0 || password.length() != 0){
+
+                //Guarda na variavel um todas as informações de um usuario caso seja encontrado pelo metodo "findByUsername" no DB
+                var user = this.userRepository.findByUsername(username);
+
+                var passwordCheck = BCrypt.verifyer().verify(password.toCharArray(),user.getPassword());
+
+                if(passwordCheck.verified){
+
+                    filterChain.doFilter(request, response);
+
+                }else{
+
+                    response.sendError(401);
+
+                }
+            
+            }else{
+
+                response.sendError(401);
+            }        
+
+        }else{
+                
+            filterChain.doFilter(request, response);
+        }
     }    
 }
